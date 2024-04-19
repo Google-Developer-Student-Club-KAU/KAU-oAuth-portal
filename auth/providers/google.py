7d346ce376a5dbe561435c7c9abfd31a96d649e0
aqasemi <alqasemy2020@gmail.com>
2024-04-19 02:15:44 +0000

    change client_secret read method

--- auth/providers/google.py
+++ auth/providers/google.py
@@ -1,15 +1,23 @@
 from google_auth_oauthlib.flow import Flow
 from flask import url_for
 import pathlib
+from json import loads
 
 import os, sys
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev | remove in production 
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"]  = "1"
 
-client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET_FILE_PATH"]
+try:
+    client_secret_config = loads(os.environ["GOOGLE_CLIENT_SECRET"])
+    flow = Flow.from_client_config(
+        client_config=client_secret_config,
+        scopes="openid profile email"
+    )
+except:
+    client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET"]
 
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes="openid profile email"
-)
+    flow = Flow.from_client_secrets_file(
+        client_secrets_file=client_secrets_file,
+        scopes="openid profile email"
+    )
