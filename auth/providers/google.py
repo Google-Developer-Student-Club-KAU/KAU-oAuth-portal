ebede2180c150c261728d92d8bdf1a30f8b3a3c6
aqasemi <alqasemy2020@gmail.com>
2024-04-19 02:28:06 +0000

    revert changes

--- auth/providers/google.py
+++ auth/providers/google.py
@@ -1,23 +1,15 @@
 from google_auth_oauthlib.flow import Flow
 from flask import url_for
 import pathlib
-from json import loads
 
 import os, sys
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev | remove in production 
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"]  = "1"
 
-try:
-    client_secret_config = loads(os.environ["GOOGLE_CLIENT_SECRET"])
-    flow = Flow.from_client_config(
-        client_config=client_secret_config,
-        scopes="openid profile email"
-    )
-except:
-    client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET"]
+client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET_FILE_PATH"]
 
-    flow = Flow.from_client_secrets_file(
-        client_secrets_file=client_secrets_file,
-        scopes="openid profile email"
-    )
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes="openid profile email"
+)
